cda96d9c335520b0e94a998e518b5ca3a985f642
Kajal-Meena <meenakajal47@gmail.com>
2024-01-08 17:56:02 +0530

    selenium commit

--- src/com/metlife/selenium/LaunchBrowser.java
+++ src/com/metlife/selenium/LaunchBrowser.java
@@ -3,16 +3,17 @@ package com.metlife.selenium;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Demo1LaunchBrowser
+public class LaunchBrowser
 {
     public static void main(String[] args)
     {
         WebDriver driver = new ChromeDriver();
-        WebDriver driver1 = new ChromeDriver();
-        driver.get("https://www.google.com");
-        driver1.get("https://www.facebook.com");
+
+        driver.get("https://www.facebook.com");
         String a = driver.getTitle();
-        System.out.println(a);
-        driver1.quit();
+        String b = driver.getPageSource();
+        driver.quit();
+        System.out.println("Title of page " + a);
+        System.out.println("page source " + b);
     }
 }
